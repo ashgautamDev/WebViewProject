9b396359a20deb1ec99f820141f5917904554564
ashishgautam1 <ashishgautam@intugine.com>
2024-03-21 20:37:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/gautam/webviewproject/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.gautam.webviewproject
+
+import android.os.Bundle
+import android.os.Message
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebView.WebViewTransport
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.databaseEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.allowContentAccess = true
+        webView.settings.allowFileAccess = true
+        webView.settings.setSupportMultipleWindows(true)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                webView.loadUrl(request.url.toString())
+                return true
+            }
+        }
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(
+                view: WebView,
+                url: String,
+                message: String,
+                result: JsResult
+            ): Boolean {
+                return super.onJsAlert(view, url, message, result)
+            }
+
+            override fun onCreateWindow(
+                view: WebView,
+                isDialog: Boolean,
+                isUserGesture: Boolean,
+                resultMsg: Message
+            ): Boolean {
+                val newWebView = WebView(this@MainActivity)
+                view.addView(newWebView)
+                val transport = resultMsg.obj as WebViewTransport
+                transport.webView = newWebView
+                resultMsg.sendToTarget()
+                return true
+            }
+        }
+        val userAgent = WebSettings.getDefaultUserAgent(this)
+        webView.settings.userAgentString = userAgent
+        webView.loadUrl("https://assist.studio")
+    }
+}
